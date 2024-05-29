abbcee49885448822f48f1c832055ae672953b1c
MavericCod <921chavanprem@gmail.com>
2024-05-29 23:27:40 +0530

    completed homework assignment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <heading>
+    <div className="container">
+      <header className="heading">
         <h1>Topics covered</h1>
         <p>The following is a list of all the topics we cover in the MDN learning area.</p>
-      </heading>
+      </header>
       <section>
-        <h2>Getting started with the web</h2>
+        <h2 id="getting-started"><a href="#getting-started">Getting started with the web</a></h2>
         <p>
-        Provides a practical introduction to web development for complete beginners.
+          Provides a practical introduction to web development for complete beginners.
         </p>
       </section>
       <section>
-        <h2>HTML — Structuring the web</h2>
+        <h2 id="html-structuring"><a href="#html-structuring">HTML — Structuring the web</a></h2>
         <p>
-        HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.
+          HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.
         </p>
       </section>
       <section>
-        <h2>CSS - Styling the web</h2>
+        <h2 id="css-styling"><a href="#css-styling">CSS - Styling the web</a></h2>
         <p>
-        CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.
+          CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.
         </p>
       </section>
     </div>
